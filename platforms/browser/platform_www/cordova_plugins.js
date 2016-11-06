943c2c44995dcc9b4b71e02e6b6e5e5a47aa8c3a
Kuba Szalaty <nnaisur@gmail.com>
2016-11-06 22:01:36 +0100

    added paper date picker; get login and pass from localestorage; remove push notif

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -618,6 +618,33 @@ module.exports = [
         "clobbers": [
             "PushNotification"
         ]
+    },
+    {
+        "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification.js",
+        "id": "de.appplant.cordova.plugin.local-notification.LocalNotification",
+        "pluginId": "de.appplant.cordova.plugin.local-notification",
+        "clobbers": [
+            "cordova.plugins.notification.local",
+            "plugin.notification.local"
+        ]
+    },
+    {
+        "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification-core.js",
+        "id": "de.appplant.cordova.plugin.local-notification.LocalNotification.Core",
+        "pluginId": "de.appplant.cordova.plugin.local-notification",
+        "clobbers": [
+            "cordova.plugins.notification.local.core",
+            "plugin.notification.local.core"
+        ]
+    },
+    {
+        "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification-util.js",
+        "id": "de.appplant.cordova.plugin.local-notification.LocalNotification.Util",
+        "pluginId": "de.appplant.cordova.plugin.local-notification",
+        "merges": [
+            "cordova.plugins.notification.local.core",
+            "plugin.notification.local.core"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -644,7 +671,9 @@ module.exports.metadata =
     "cordova-plugin-statusbar": "2.1.3",
     "cordova-plugin-vibration": "2.1.2",
     "cordova-plugin-whitelist": "1.2.2",
-    "phonegap-plugin-push": "1.8.4"
+    "phonegap-plugin-push": "1.8.4",
+    "cordova-plugin-app-event": "1.2.0",
+    "de.appplant.cordova.plugin.local-notification": "0.8.4"
 }
 // BOTTOM OF METADATA
 });
--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -39,116 +39,123 @@
       var time;
       var eventsList = [];
       connect.addEventListener('click',function(){
-        // app.playVibrate();
-        $('.result2').html('');
-        // testowe dane
-        // var tableHtml = "<tbody><tr class=\"gridDaneHead\" valign=\"bottom\"><td>Data zajęć</td><td>Od</td><td>Do</td><td>Przedmiot</td><td>Prowadzący</td><td>Sala</td><td>Adres budynku</td><td>Forma zajęć nazwa</td><td>Forma zalicz.</td></tr><tr class=\"gridDane\"><td>31.10.2016 poniedziałek</td><td>08:15</td><td>10:00</td><td>Podstawy ochrony informacji</td><td><a id=\"lb_Prowadzacy0\" class=\"opisPrzedmDyd\" onclick=\"pokazOpis(1, '0')\">dr hab.inż. Jerzy Pejaś</a></td><td>WI WI2- 227</td><td>ul. Żołnierska 52</td><td>wykład</td><td>egzamin</td></tr><tr class=\"gridDane\"><td>31.10.2016 poniedziałek</td><td>10:15</td><td>12:00</td><td>Komunikacja człowiek-komputer</td><td><a id=\"lb_Prowadzacy1\" class=\"opisPrzedmDyd\" onclick=\"pokazOpis(1, '1')\">dr inż. Adam Nowosielski</a></td><td>WI WI2- 300</td><td>ul. Żołnierska 52</td><td>laboratorium</td><td>ocena</td></tr><tr class=\"gridDane\"><td>31.10.2016 poniedziałek</td><td>12:15</td><td>14:00</td><td>LaTeX - system składu tekstów inżynierskich - Przedmiot obieralny II</td><td><a id=\"lb_Prowadzacy2\" class=\"opisPrzedmDyd\" onclick=\"pokazOpis(1, '2')\">dr inż. Remigiusz Olejnik</a></td><td>WI WI2- 126</td><td>ul. Żołnierska 52</td><td>wykład</td><td>ocena</td></tr><tr class=\"gridDane\"><td>31.10.2016 poniedziałek</td><td>18:15</td><td>20:00</td><td>LaTeX - system składu tekstów inżynierskich - Przedmiot obieralny II</td><td><a id=\"lb_Prowadzacy3\" class=\"opisPrzedmDyd\" onclick=\"pokazOpis(1, '3')\">dr inż. Remigiusz Olejnik</a></td><td>WI WI1- 011</td><td>ul. Żołnierska 49</td><td>laboratorium</td><td>ocena</td></tr><tr class=\"gridDane\"><td>03.11.2016 czwartek</td><td>10:15</td><td>12:00</td><td>Systemy internetowe</td><td><a id=\"lb_Prowadzacy4\" class=\"opisPrzedmDyd\" onclick=\"pokazOpis(1, '4')\">dr hab. Izabela Rejer</a></td><td>WI WI2- 227</td><td>ul. Żołnierska 52</td><td>wykład</td><td>ocena</td></tr><tr class=\"gridDane\"><td>04.11.2016 piątek</td><td>12:15</td><td>14:00</td><td>Podstawy transmisji danych</td><td><a id=\"lb_Prowadzacy5\" class=\"opisPrzedmDyd\" onclick=\"pokazOpis(1, '5')\">dr inż. Tomasz Mąka</a></td><td>WI WI2- 201</td><td>ul. Żołnierska 52</td><td>laboratorium</td><td>ocena</td></tr><tr class=\"gridDane\"><td>04.11.2016 piątek</td><td>14:15</td><td>16:00</td><td>Wstęp do sztucznej inteligencji</td><td><a id=\"lb_Prowadzacy6\" class=\"opisPrzedmDyd\" onclick=\"pokazOpis(1, '6')\">mgr inż. Marcin Pietrzykowski</a></td><td>WI WI1- 307</td><td>ul. Żołnierska 49</td><td>laboratorium</td><td>ocena</td></tr></tbody>";
-        // $table = $(tableHtml);
-
-        $.get('https://edziekanat.zut.edu.pl/WU/PodzGodzin.aspx', function( data ) {
-          // $('.result').append('<p>Pobrano strone logowania</p>');
-          console.log('Pobrano strone logowania');
-          // pozniej result zamien na wirtualny div
-          $result = $('<div>');
-          // $result = $('.result');
-          var login = $('#login').val();
-          var password = $('#password').val();
+        app.updateUserData();
+      },true);
 
 
+      app.setupStatusBar();
 
+
+    },
+    setupStatusBar: function() {
+
+      StatusBar.backgroundColorByHexString('#3f51b5');
+
+    },
+
+    updateUserData: function(){
+
+      $('.result2').html('');
+
+      $.get('https://edziekanat.zut.edu.pl/WU/PodzGodzin.aspx', function( data ) {
+        console.log('Pobrano strone logowania');
+        $result = $('<div>');
+        var userData = JSON.parse(localStorage.userData);
+
+        var login = userData.login;
+        var password = userData.password;
+
+        if(!login || !password ){
+          console.log('Błędne dane');
+          return;
+        }
+
+        $result.html(data);
+        $form = $result.find('form');
+        $form.find('#ctl00_ctl00_ContentPlaceHolder_MiddleContentPlaceHolder_txtIdent').val(login);
+        $form.find('#ctl00_ctl00_ContentPlaceHolder_MiddleContentPlaceHolder_txtHaslo').val(password);
+
+        var action = 'https://edziekanat.zut.edu.pl/WU/' + $form.attr('action');
+        var serialized = $form.serialize();
+
+        serialized += "&ctl00%24ctl00%24ContentPlaceHolder%24MiddleContentPlaceHolder%24butLoguj=Zaloguj";
+
+        time = new Date();
+        var ajax = $.ajax({
+          type: "POST",
+          url: action,
+          data: serialized,
+        }).done(function(data, textStatus, request){
+          console.log('Zalogowano');
           $result.html(data);
+
+          var name = $result.find('#ctl00_ctl00_ContentPlaceHolder_wumasterWhoIsLoggedIn').text().replace(/ –.*/,'');
+
+
           $form = $result.find('form');
-          $form.find('#ctl00_ctl00_ContentPlaceHolder_MiddleContentPlaceHolder_txtIdent').val(login);
-          $form.find('#ctl00_ctl00_ContentPlaceHolder_MiddleContentPlaceHolder_txtHaslo').val(password);
 
-          var action = 'https://edziekanat.zut.edu.pl/WU/' + $form.attr('action');
-          var serialized = $form.serialize();
+          action = 'https://edziekanat.zut.edu.pl/WU/' + $form.attr('action');
 
-          serialized += "&ctl00%24ctl00%24ContentPlaceHolder%24MiddleContentPlaceHolder%24butLoguj=Zaloguj";
+          serialized = $form.serialize();
+          serialized = serialized.replace('Tygodniowo','Semestralnie');
+          serialized = serialized.replace('__EVENTTARGET=&','__EVENTTARGET=ctl00%24ctl00%24ContentPlaceHolder%24RightContentPlaceHolder%24rbJak%242&');
 
-          time = new Date();
-          // http://edziekanat.zut.edu.pl/WU/PodzGodzDruk.aspx?typ=ics&from=20160930&to=20170226&sesja=0&sig=v5FA2qUuBECvGc52TVuBTGtmq5o%3d
           var ajax = $.ajax({
             type: "POST",
             url: action,
             data: serialized,
           }).done(function(data, textStatus, request){
-            // $('.result').append('<p>Zalogowano</p>');
-            console.log('Zalogowano');
             $result.html(data);
+            console.log('Pobrano!');
+            $table = $result.find('#ctl00_ctl00_ContentPlaceHolder_RightContentPlaceHolder_dgDane');
 
-            var name = $result.find('#ctl00_ctl00_ContentPlaceHolder_wumasterWhoIsLoggedIn').text().replace(/ –.*/,'');
+            var keys = [];
 
-            document.querySelector('main-app').set('user.logged', true);
-            document.querySelector('main-app').set('user.name', name);
+            // polskie znaki i spacje nie daja rady jako klucze w polymerze jest problem pozniej
+            // wiec łatwiej jest to zrobić recznie niz pisac do tego algorytm lel
+            // $table.find('.gridDaneHead').children().map(function(){
+            //   keys.push($(this).text());
+            // });
 
-            $form = $result.find('form');
-
-            action = 'https://edziekanat.zut.edu.pl/WU/' + $form.attr('action');
-
-            serialized = $form.serialize();
-            serialized = serialized.replace('Tygodniowo','Semestralnie');
-            serialized = serialized.replace('__EVENTTARGET=&','__EVENTTARGET=ctl00%24ctl00%24ContentPlaceHolder%24RightContentPlaceHolder%24rbJak%242&');
-
-            var ajax = $.ajax({
-              type: "POST",
-              url: action,
-              data: serialized,
-            }).done(function(data, textStatus, request){
-              $result.html(data);
-              // $('.result').append('<p>Pobrano!</p>');
-              console.log('Pobrano!');
-              // $('.result2').html(data);
-              // $('.result2').html($result.find('#ctl00_ctl00_ContentPlaceHolder_RightContentPlaceHolder_dgDane').html());
-              $table = $result.find('#ctl00_ctl00_ContentPlaceHolder_RightContentPlaceHolder_dgDane');
-
-              var keys = [];
-
-              // polskie znaki i spacje nie daja rady jako klucze w polymerze jest problem pozniej
-              // wiec łatwiej jest to zrobić recznie niz pisac do tego algorytm lel
-              // $table.find('.gridDaneHead').children().map(function(){
-              //   keys.push($(this).text());
-              // });
-
-              keys = ["Data_zajac", "Od", "Do", "Przedmiot", "Prowadzacy", "Sala", "Adres_budynku", "Forma_zajec_nazwa", "Forma_zalicz"];
+            keys = ["Data_zajac", "Od", "Do", "Przedmiot", "Prowadzacy", "Sala", "Adres_budynku", "Forma_zajec_nazwa", "Forma_zalicz"];
 
 
-              var elements = [];
+            var elements = [];
 
-              $table.find('.gridDane').map(function(elemIndex){
-                var elem = $(this);
-                var obj = {};
-                elements.push(obj);
+            $table.find('.gridDane').map(function(elemIndex){
+              var elem = $(this);
+              var obj = {};
+              elements.push(obj);
 
-                elem.children().map(function(index){
+              elem.children().map(function(index){
 
-                  obj[keys[index]] = $(this).text();
-
-                });
+                obj[keys[index]] = $(this).text();
 
               });
 
-              $('paper-card-repeater').prop('elements',elements);
-
-
-              navigator.notification.alert('Pobrano najnowszy plan');
-
-
             });
 
-          }).fail(function(data) {
-            // $('.result').append('<p>Niezalogowano</p>');
-            console.log('Niezalogowano');
+            $('paper-card-repeater').prop('elements',elements);
+
+            document.querySelector('main-app').set('user.logged', true);
+            document.querySelector('main-app').set('user.name', name);
+            document.querySelector('main-app').set('user.lastCheck', new Date().toLocaleString());
+
+            document.querySelector('main-app').toastMsg('Pobrano najnowszy plan');
+
+
           });
 
-
-        }).fail(function() {
-          // $('.result').append('<p>Błąd</p>');
-          console.log('Błąd');
+        }).fail(function(data) {
+          // $('.result').append('<p>Niezalogowano</p>');
+          console.log('Niezalogowano');
         });
 
-        // console.log('vibracje');
-      },true);
+
+      }).fail(function() {
+        console.log('Błąd');
+      });
+
 
 
     },
