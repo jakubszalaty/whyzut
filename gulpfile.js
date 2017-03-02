5b8b8cb642aa5cd47b865522e07fec64eeb1ee0e
Kuba Szalaty <jakub.szalaty@gmail.com>
2017-03-02 13:49:42 +0100

    Added end time and change label position

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,25 @@
 const gulp = require('gulp')
 const vulcanize = require('gulp-vulcanize')
 
-gulp.task('vulcanize', function() {
-  return gulp.src('www/webelements/main-app/main-app.html')
-    .pipe(vulcanize({
-      stripComments: true,
-      inlineScripts: true,
-      inlineCss: true
-    }))
-    .pipe(gulp.dest(`${__dirname}/www/dist/`))
+gulp.task('vulcanize', function () {
+    return gulp.src('www/webelements/main-app/main-app.html')
+        .pipe(vulcanize({
+            stripComments: true,
+            inlineScripts: true,
+            inlineCss: true
+        }))
+        .pipe(gulp.dest(`${__dirname}/www/dist/`))
 })
 
-gulp.task('moveAssets', function() {
-  return gulp.src([
-    'www/bower_components/webcomponentsjs/webcomponents-lite.js',
-    'www/bower_components/moment/moment.js',
-    'www/bower_components/moment/min/moment-with-locales.js',
-    'www/bower_components/moment/locale/pl.js',
-    'www/bower_components/jquery/dist/jquery.js',
-  ])
-  .pipe(gulp.dest(`${__dirname}/www/dist/`))
+gulp.task('moveAssets', function () {
+    return gulp.src([
+        'www/bower_components/webcomponentsjs/webcomponents-lite.js',
+        'www/bower_components/moment/moment.js',
+        'www/bower_components/moment/min/moment-with-locales.js',
+        'www/bower_components/moment/locale/pl.js',
+        'www/bower_components/jquery/dist/jquery.js',
+    ])
+        .pipe(gulp.dest(`${__dirname}/www/dist/`))
 })
 
-gulp.task('default', ['vulcanize','moveAssets'])
+gulp.task('default', ['vulcanize', 'moveAssets'])
